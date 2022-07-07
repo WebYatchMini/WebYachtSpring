ea6db07a2ff86bf1c3840f4e347b5a8a5510f40b
koojawon <jawonkoo@naver.com>
2022-07-07 23:22:32 +0900

    remove cache

--- src/main/frontend/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { legacy_createStore as createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const create = (reducers) => {
-  return createStore(reducers, composeWithDevTools());
-  // return createStore(reducers);
-}
-
-export default create;
